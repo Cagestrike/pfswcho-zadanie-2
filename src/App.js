883825b42971e29c21954b5d23a5d81293d140b7
Damian Ciechan <cagestrike@gmail.com>
2022-06-09 20:34:05 +0200

    update app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 666 <code>src/App.js</code> and save to reload.
+          Damian <code>Ciechan</code><br/>
+          PFSWCHO Zadanie 2
         </p>
         <a
           className="App-link"
